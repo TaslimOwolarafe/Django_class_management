dc8cc0d27fa31c73a4c6d92b832828bb19eb1fd3
TaslimOwolarafe <owolarafetaslim@gmail.com>
2022-09-09 15:47:20 +0100

    created login, home

---
+++ backend/classApp/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import login, authenticate
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm 
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=255, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user or not user.is_active:
+            raise forms.ValidationError("Sorry, that login was invalid. Please try again.")
+        return self.cleaned_data
+
+    def login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        return user
--- backend/classApp/views.py
+++ backend/classApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from . forms import LoginForm
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if request.POST and form.is_valid():
+        user = form.login(request)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect("users/home.html")
+    return render(request, 'registration/login.html', {'login_form': form })
+
+def home(request):
+    return render(request, "users/home.html")
